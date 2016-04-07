88b37be2ef76b78ef2d6020955fab1cf5df043bc
Stefano De Cillis <Steve@MacBook-Pro-di-Stefano-2.local>
2016-04-07 18:35:55 +0200

    ***

--- analisi.cpp
+++ analisi.cpp
@@ -1,7 +1,10 @@
 #include "analisi.h"
 #include "ui_analisi.h"
 #include <menu.h>
-#include <iostream>
+#include <vector>
+#include <QDate>
+#include <struttura_dati.h>
+
 
 Analisi::Analisi(QWidget *parent) :
     QMainWindow(parent),
@@ -18,7 +21,40 @@ Analisi::~Analisi()
 void Analisi::on_exit_button_clicked()
 {
     Menu* t = new Menu();
-    std::cout << "BOOHM";
     t->show();
     this->close();
+}
+
+void Analisi::on_button_dati_clicked()
+{
+   if (!(ui->take_threshold->text().isEmpty())) //Se non è vuota
+   {
+       //Lavora
+   }
+}
+
+std::vector<QDate> Analisi::get_threshold(std::string user, double threshold)
+{
+    Struttura_dati::sort_vect(Struttura_dati::Wreading,user);   //Sorting
+    std::vector<water_reading*> consum_user = Struttura_dati::score_ranges(Struttura_dati::Wreading.at(user));   //Acquisto vettore letture
+    std::vector<QDate> loss_consum;   //inizializzo vector che conterra le date dei consumi
+    bool lossFound = false;
+    for (size_t i = 0; i < consum_user.size(); i++)
+    {
+        if ((consum_user[i]->get_data().tm_hour >= 0 && consum_user[i]->get_data().tm_hour <= 5) && lossFound == false)  //Se è compreso tra le 00.00 e le 05.00
+        {
+            if (consum_user[i]->get_consumption() > threshold)
+            {
+                lossFound = true;
+                QDate data(consum_user[i]->get_data().tm_year,consum_user[i]->get_data().tm_mon,consum_user[i]->get_data().tm_mday); //creo QDate
+                loss_consum.push_back(data); //inserisco QDate nel vector
+            }
+        }
+        if ((consum_user[i]->get_data().tm_hour >= 6 && consum_user[i]->get_data().tm_hour <= 23) && consum_user[i]->get_data().tm_mday != consum_user[i+1]->get_data().tm_mday)
+        {
+            lossFound = false;  //Se supera le ore notturne oppure cambia giorno, ripristino tutto
+        }
+    }
+
+  return loss_consum;
 }
--- analisi.h
+++ analisi.h
@@ -14,8 +14,12 @@ class Analisi : public QMainWindow
 public:
     explicit Analisi(QWidget *parent = 0);
     ~Analisi();
+    static std::vector<QDate> get_threshold(std::string user, double threshold);
 public slots:
     void on_exit_button_clicked();
+
+private slots:
+    void on_button_dati_clicked();
 
 private:
     Ui::Analisi *ui;
