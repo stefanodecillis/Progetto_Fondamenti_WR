ef4656d53550d40a753754e40fb0ebcec8254a3e
Alex <alexfmm95@gmail.com>
2016-04-07 19:35:49 +0200

    dada

--- analisi.cpp
+++ analisi.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 #include <QDate>
 #include <struttura_dati.h>
-
+#include<qstring.h>
+#include<qdebug.h>
 
 Analisi::Analisi(QWidget *parent) :
     QMainWindow(parent),
@@ -29,12 +30,33 @@ void Analisi::on_button_dati_clicked()
 {
    if (!(ui->take_threshold->text().isEmpty())) //Se non è vuota
    {
+
+
        //Lavora
-   }
+
+       for(int i=0;i<Struttura_dati::index.size();i++){//per ogni ID utente presente
+        std::vector<QDate> temp;
+        bool ok = false;
+
+        temp=get_threshold(Struttura_dati::index.at(i),ui->take_threshold->text().toDouble(&ok));
+        //stampo nella cosa
+        if(temp.size()==0){
+            //niente.
+        }else{//stampa
+        for(int i=0;i<temp.size();i++){
+ui->list_perdite->addItem("ciao");
+}
+        }
+
+       }
+
+   }//fine if principale
 }
 
 std::vector<QDate> Analisi::get_threshold(std::string user, double threshold)
 {
+
+
     Struttura_dati::sort_vect(Struttura_dati::Wreading,user);   //Sorting
     std::vector<water_reading*> consum_user = Struttura_dati::score_ranges(Struttura_dati::Wreading.at(user));   //Acquisto vettore letture
     std::vector<QDate> loss_consum;   //inizializzo vector che conterra le date dei consumi
@@ -43,15 +65,20 @@ std::vector<QDate> Analisi::get_threshold(std::string user, double threshold)
     {
         if ((consum_user[i]->get_data().tm_hour >= 0 && consum_user[i]->get_data().tm_hour <= 5) && lossFound == false)  //Se è compreso tra le 00.00 e le 05.00
         {
-            if (consum_user[i]->get_consumption() > threshold)
+             qDebug()<<"dentro primo if";
+            if (consum_user[i]->get_consumption() >= threshold)
             {
+                qDebug()<<"dentro secondo ig";
                 lossFound = true;
                 QDate data(consum_user[i]->get_data().tm_year,consum_user[i]->get_data().tm_mon,consum_user[i]->get_data().tm_mday); //creo QDate
                 loss_consum.push_back(data); //inserisco QDate nel vector
             }
         }
-        if ((consum_user[i]->get_data().tm_hour >= 6 && consum_user[i]->get_data().tm_hour <= 23) && consum_user[i]->get_data().tm_mday != consum_user[i+1]->get_data().tm_mday)
+        //if ((consum_user[i]->get_data().tm_hour >= 6 && consum_user[i]->get_data().tm_hour <= 23) )
+        if (consum_user[i]->get_data().tm_hour >= 6 && consum_user[i]->get_data().tm_hour <= 23)
         {
+          qDebug()<<"diventafalse";
+           // && consum_user[i]->get_data().tm_mday != consum_user[i+1]->get_data().tm_mday
             lossFound = false;  //Se supera le ore notturne oppure cambia giorno, ripristino tutto
         }
     }
